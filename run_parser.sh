fd5173ea8b7283eaf2b4ae1660f6bd64a1baa641
Wouter van Atteveldt <wouter@vanatteveldt.com>
2016-04-24 17:44:35 +0200

    Allow partial running of pipeline

--- run_parser.sh
+++ run_parser.sh
@@ -13,8 +13,12 @@ TDIR=$NEWSREADER_HOME/tools
 
 . $NEWSREADER_HOME/newsreader-env/bin/activate
 
-java -jar $MDIR/ixa-pipe-tok/target/ixa-pipe-tok-1.8.4.jar tok -l nl |\
-  $MDIR/morphosyntactic_parser_nl/run_parser.sh |\
+function parse {
+    java -jar $MDIR/ixa-pipe-tok/target/ixa-pipe-tok-1.8.4.jar tok -l nl |\
+      $MDIR/morphosyntactic_parser_nl/run_parser.sh
+}
+
+function annotate {
   java -jar $MDIR/ixa-pipe-nerc/target/ixa-pipe-nerc-1.6.0-exec.jar tag -m $TDIR/nerc-models-1.5.4/nl/nl-6-class-clusters-sonar.bin |\
   java -jar $MDIR/ixa-pipe-ned/target/ixa-pipe-ned-1.1.4.jar -p 2060 |\
   python $MDIR/dbpedia_ner/dbpedia_ner.py |\
@@ -25,4 +29,19 @@ java -jar $MDIR/ixa-pipe-tok/target/ixa-pipe-tok-1.8.4.jar tok -l nl |\
   bash $MDIR/OntoTagger/scripts/srl-framenet-tagger.sh |\
   bash $MDIR/OntoTagger/scripts/nominal-events.sh |\
   python $MDIR/vua-srl-dutch-nominal-events/vua-srl-dutch-additional-roles.py |\
+  bash $MDIR/EventCoreference/scripts/event-coreference-nl.sh |\
   cat
+}
+
+WHAT=${1:-all}
+
+if [ $WHAT = "all" ]; then
+    parse | annotate
+elif [ $WHAT = "parse" ]; then
+    parse
+elif [ $WHAT = "annotate" ]; then
+    annotate
+else 
+    echo "Unknown argument: $WHAT"
+    exit 1
+fi
